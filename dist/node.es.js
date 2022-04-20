93426172314b40f36e53baafde40ab6898e866cb
codehyouka <plants.coordinators@gmail.com>
2022-04-20 17:35:14 +0800

    Remove unnecessary files

--- dist/node.es.js
+++
@@ -1,8 +0,0 @@
-import {default as agenttype_module} from '../src/module/agenttype/index';
-import {default as dom_module} from '../src/module/dom/index';
-import {default as domready_module} from '../src/module/domready/index';
-import {default as http_module} from '../src/module/http/index';
-export const agenttype=agenttype_module;
-export const dom=dom_module;
-export const domready=domready_module;
-export const http=http_module;
