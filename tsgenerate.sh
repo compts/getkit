e3d24c418432e61ad4ec1d58801c777b60d2e478
codehyouka <plants.coordinators@gmail.com>
2024-12-06 18:31:00 +0800

    remove comment and fix TS config

--- tsgenerate.sh
+++ tsgenerate.sh
@@ -1,1 +1,1 @@
-npx -p typescript tsc dist/esm/src/module/main.js --declaration --allowJs --emitDeclarationOnly --outDir dist/ts
+npx -p typescript tsc dist/esm/src/module/*.js --declaration --allowJs --emitDeclarationOnly --outDir dist/ts
