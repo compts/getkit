99f21679a0c7b68ce3d86141e768fdda206fbd15
codehyouka <plants.coordinators@gmail.com>
2025-09-15 20:03:39 +0800

    Update the code base

--- src/adapter/local_ws.js
+++ src/adapter/local_ws.js
@@ -1,4 +1,4 @@
-const {each, getTypeof, isEmpty} = require("structkit");
+const {toBoolean, isEmpty} = require("structkit");
 
 /**
  * Initiation of web websocket
@@ -22,35 +22,49 @@ function LocalWs (api, config, subMethod) {
 
         definePath = definePath+ ':' + config.port;
 
+    } else {
+
+        definePath = definePath+ ':' + (api.isHttps
+            ? 443
+            : 80);
+
     }
+
 
     this.ws = new api.ClassSocket(definePath);
 
     this.ws.onopen = () => {
-        this.readyState = this.ws.readyState ===1;
+
+        this.readyState = toBoolean(this.ws.readyState);
         console.log('Connected to WebSocket server');
+
     };
 
     this.ws.onmessage = (event) => {
-    //    console.log('Received message:', event.data);
+
+        //    console.log('Received message:', event.data);
         subMethod.onmessage(event.data);
+
     };
 
     this.ws.onclose = () => {
 
-        this.readyState = this.ws.readyState===1;
+        this.readyState = toBoolean(this.ws.readyState);
         console.log('Disconnected from WebSocket server');
+
     };
 
     this.ws.onerror = (error) => {
+
         console.error('WebSocket error:', error);
+
     };
-    this.readyState = this.ws.readyState===1;
+
+    this.readyState = toBoolean(this.ws.readyState);
 
     return this;
 
 }
-
 
 
 module.exports = LocalWs;
